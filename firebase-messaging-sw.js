aa6562729346d2b7da7e5ac661e8ae4cc56fe60f
diesel8603 <dieselllllllll89@gmail.com>
2025-08-08 01:42:03 +0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBXXXCLGe70id54wlMhUtHQHOJe8l4a6wA",
+  authDomain: "live-chat-9d81c.firebaseapp.com",
+  projectId: "live-chat-9d81c",
+  messagingSenderId: "253242248304",
+  appId: "1:253242248304:web:3e440995fe27bc8e2fb8b5"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Arka plan mesajı alındı ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/1037/1037916.png',
+    vibrate: [200, 100, 200],
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
