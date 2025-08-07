358d891e8514bfe94456ae301503a7acf1f7a498
diesel8603 <dieselllllllll89@gmail.com>
2025-08-08 01:14:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,20 @@
-const CACHE_NAME = 'hatirlatici-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap',
-  'https://actions.google.com/sounds/v1/alarms/digital_watch_alarm_long.ogg'
-];
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  if (event.action === 'confirm') {
+    event.waitUntil(
+      clients.matchAll({type: 'window'}).then(windowClients => {
+        for (const client of windowClients) {
+          client.postMessage({type: 'confirm'});
+          client.focus();
+          return;
+        }
+        clients.openWindow('/');
+      })
+    );
+  } else {
+    event.waitUntil(
+      clients.openWindow('/')
+    );
+  }
 });
