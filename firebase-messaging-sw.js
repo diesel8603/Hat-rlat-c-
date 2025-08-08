829244b72efeaab5786fa4d8313c653869dac56f
diesel8603 <dieselllllllll89@gmail.com>
2025-08-08 21:47:26 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,25 @@
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyB0wYo4y9xi1lZSwi02iJQ7v0iZ-nq8y-U",
   authDomain: "chat-f5f0c.firebaseapp.com",
+  databaseURL: "https://chat-f5f0c-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "chat-f5f0c",
   storageBucket: "chat-f5f0c.firebasestorage.app",
   messagingSenderId: "571398938754",
-  appId: "1:571398938754:web:147a9e1d0106e8a0a4d49b"
-});
+  appId: "1:571398938754:web:147a9e1d0106e8a0a4d49b",
+  measurementId: "G-CFZ8PWP6DY"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
+  console.log('[SW] Arka planda bildirim:', payload);
+  self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: '/icon.png',
-    vibrate: [200, 100, 200]
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    icon: payload.notification.icon || 'icon.png'
+  });
 });
